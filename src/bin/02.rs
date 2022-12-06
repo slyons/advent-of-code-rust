8131480a600f33cb1f8084554ac5f145cec70c09
Scott Lyons <scottalyons@gmail.com>
2022-12-05 19:56:43 -0800

    Day 2, part 2 complete

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,7 +1,67 @@
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+
+#[derive(PartialEq, Eq, Hash, Debug)]
 pub enum RPS {
     ROCK,
     PAPER,
     SCISSORS,
+}
+
+lazy_static! {
+    static ref SHAPE_SCORES: HashMap<RPS, u32> = {
+        let mut m = HashMap::new();
+        m.insert(RPS::ROCK, 1);
+        m.insert(RPS::PAPER, 2);
+        m.insert(RPS::SCISSORS, 3);
+        m
+    };
+}
+
+#[derive(PartialEq, Eq, Hash, Debug)]
+pub enum WLD {
+    WIN,
+    LOSE,
+    DRAW,
+}
+
+lazy_static! {
+    static ref SHAPE_WLD: HashMap<RPS, HashMap<WLD, RPS>> = {
+        let mut shapes = HashMap::new();
+        let mut outcomes = HashMap::new();
+
+        //Rock
+        outcomes.insert(WLD::WIN, RPS::PAPER);
+        outcomes.insert(WLD::DRAW, RPS::ROCK);
+        outcomes.insert(WLD::LOSE, RPS::SCISSORS);
+        shapes.insert(RPS::ROCK, outcomes);
+
+        let mut outcomes = HashMap::new();
+        //Paper
+        outcomes.insert(WLD::WIN, RPS::SCISSORS);
+        outcomes.insert(WLD::DRAW, RPS::PAPER);
+        outcomes.insert(WLD::LOSE, RPS::ROCK);
+        shapes.insert(RPS::PAPER, outcomes);
+
+        let mut outcomes = HashMap::new();
+        //Scissors
+        outcomes.insert(WLD::WIN, RPS::ROCK);
+        outcomes.insert(WLD::DRAW, RPS::SCISSORS);
+        outcomes.insert(WLD::LOSE, RPS::PAPER);
+        shapes.insert(RPS::SCISSORS, outcomes);
+        shapes
+    };
+}
+
+fn round_score(them: &RPS, us: &RPS) -> u32 {
+    match (them, us) {
+        // We win
+        (RPS::ROCK, RPS::PAPER) | (RPS::PAPER, RPS::SCISSORS) | (RPS::SCISSORS, RPS::ROCK) => 6,
+        // Tie
+        (RPS::ROCK, RPS::ROCK) | (RPS::PAPER, RPS::PAPER) | (RPS::SCISSORS, RPS::SCISSORS) => 3,
+        // We lost
+        _ => 0,
+    }
 }
 
 pub fn parse_rps(input: &str) -> Vec<(RPS, RPS)> {
@@ -27,37 +87,43 @@ pub fn parse_rps(input: &str) -> Vec<(RPS, RPS)> {
         .collect()
 }
 
+pub fn parse_wld(input: &str) -> Vec<(RPS, WLD)> {
+    parse_rps(input)
+        .into_iter()
+        .map(|(them, us)| match us {
+            RPS::ROCK => (them, WLD::LOSE),
+            RPS::PAPER => (them, WLD::DRAW),
+            RPS::SCISSORS => (them, WLD::WIN),
+        })
+        .collect()
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
     let strat = parse_rps(input);
     Some(
         strat
             .iter()
-            .map(|round| {
-                let shape_score = match round.1 {
-                    RPS::ROCK => 1,
-                    RPS::PAPER => 2,
-                    RPS::SCISSORS => 3,
-                };
-                let round_score = match round {
-                    // We win
-                    (RPS::ROCK, RPS::PAPER)
-                    | (RPS::PAPER, RPS::SCISSORS)
-                    | (RPS::SCISSORS, RPS::ROCK) => 6,
-                    // Tie
-                    (RPS::ROCK, RPS::ROCK)
-                    | (RPS::PAPER, RPS::PAPER)
-                    | (RPS::SCISSORS, RPS::SCISSORS) => 3,
-                    // We lost
-                    _ => 0,
-                };
+            .map(|(them, us)| {
+                let shape_score = SHAPE_SCORES[us];
+                let round_score = round_score(them, us);
                 shape_score + round_score
             })
             .sum(),
     )
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    Some(
+        parse_wld(input)
+            .iter()
+            .map(|(them, outcome)| {
+                let our_shape = &SHAPE_WLD[them][outcome];
+                let shape_score = SHAPE_SCORES[our_shape];
+                let round_scorez = round_score(them, our_shape);
+                shape_score + round_scorez
+            })
+            .sum(),
+    )
 }
 
 fn main() {
@@ -79,6 +145,15 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 2);
-        assert_eq!(part_two(&input), None);
+        let wld = parse_wld(&input);
+        assert_eq!(
+            wld,
+            vec!(
+                (RPS::ROCK, WLD::DRAW),
+                (RPS::PAPER, WLD::LOSE),
+                (RPS::SCISSORS, WLD::WIN)
+            )
+        );
+        assert_eq!(part_two(&input), Some(12));
     }
 }
